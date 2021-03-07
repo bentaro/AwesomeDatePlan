6c57920d11e2a6360a8e9ea213bf7fa94a18306d
Koichiro Kanaya <abcs516tk1@gmail.com>
2021-03-07 21:57:47 +0900

    implented mapPlan.vue. deleted inspire.vue

---
+++ pages/mapPlan.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+	<h1 style="font-size:35px">デートプラン</h1>
+	<v-container fluid>
+
+		<v-row>
+			<v-col cols="3">
+				<timeTable />
+			</v-col>
+			<v-col cols="1"></v-col>
+
+			<v-col cols="7">
+				<MapPanel />
+			</v-col>
+			<v-col cols="1"></v-col>
+
+		</v-row>
+
+	</v-container>
+</div>
+</template>
+
+<script>
+import MapPanel from "../components/MapPanel";
+import timeTable from "../components/timeTable";
+
+export default {
+	components: {
+		MapPanel,
+		timeTable,
+	}
+}
+</script>
